4e2dce05089460a6c871d700e5fb5570518a8d56
tom-fallon <tomfallon3@aol.com>
2015-11-08 00:45:14 +0000

    adding master module to enable site wide modules

--- core/install.sh
+++ core/install.sh
@@ -1,4 +1,4 @@
-drush si standard -y --db-url=mysql://root:123@127.0.0.1/free-paragraphs --site-name=core --account-name=administrator --account-pass=password --notify
-drush en master_core -y
+drush si standard -y --db-url=mysql://free-user:password@127.0.0.1/free-paragraphs --site-name=core --account-name=administrator --account-pass=password --notify
+drush en free_paragraphs_master_core -y
 drush fra -y
 drush cc all
