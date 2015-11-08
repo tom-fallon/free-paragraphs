30b7e623bc458845324c641ab8354d085e1aabec
tom-fallon <tomfallon3@aol.com>
2015-11-08 01:39:58 +0000

    adding bootstrap theme and sub theme

---
+++ core/sites/all/themes/bootstrap_free_paragraphs/Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.initConfig({
+		watch: {
+			less: {
+				files: ['less/*.less'],
+				tasks: ['less:development'],
+			},
+		},
+		less: {
+			development: {
+				files: {
+					'css/style.css': 'less/style.less'
+				}
+			},
+		},
+	});
+	grunt.registerTask('default', ['watch']);
+};
---
+++ core/sites/all/themes/bootstrap_free_paragraphs/README.txt
@@ -0,0 +1,7 @@
+To Install the tools required to compile the less.
+
+In the themes directory run
+npm install
+grunt
+
+This will start a watch task to compile the less.
